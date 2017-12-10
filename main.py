4f6f94bcdbc21cf2d34070a7691f681e164c8fc5
Matthew Younkins <matthewyounkins@protonmail.com>
2017-12-10 13:53:08 -0800

    Corrected some mistakes in main.py

--- main.py
+++ main.py
@@ -27,8 +27,7 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
+	
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
@@ -36,16 +35,14 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
 
-
-    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
-    graph = tf.get_default_graph()
-    w1 = tf.get_default_graph().get_tensor_by_name(vgg_input_tensor_name)
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)  #Loads model and weights
+	
+	#Layers...
+    w1   = tf.get_default_graph().get_tensor_by_name(vgg_input_tensor_name)
     keep = tf.get_default_graph().get_tensor_by_name(vgg_keep_prob_tensor_name)
-    l3 = tf.get_default_graph().get_tensor_by_name(vgg_layer3_out_tensor_name)
-    l4 = tf.get_default_graph().get_tensor_by_name(vgg_layer4_out_tensor_name)
-    l7 = tf.get_default_graph().get_tensor_by_name(vgg_layer7_out_tensor_name)
-
-
+    l3   = tf.get_default_graph().get_tensor_by_name(vgg_layer3_out_tensor_name)
+    l4   = tf.get_default_graph().get_tensor_by_name(vgg_layer4_out_tensor_name)
+    l7   = tf.get_default_graph().get_tensor_by_name(vgg_layer7_out_tensor_name)
 
     return w1, keep, l3, l4, l7
 tests.test_load_vgg(load_vgg, tf)
@@ -61,27 +58,27 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify 
     :return: The Tensor for the last layer of output 
     """
-    layer7Convolv = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding= 'same', 
-                                   kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    layer7Convolv   = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding= 'same', 
+                                       kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
 								   
     layer4Transpose = tf.layers.conv2d_transpose(layer7Convolv, num_classes, 4, strides= (2, 2), padding= 'same', 
                                                  kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
-												 
-    layer4Convolv = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding= 'same', 
-                                   kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+
+    layer4Convolv   = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding= 'same', 
+                                       kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
     
-    layer4Total = tf.add(layer4Transpose, layer4Convolv)
+    layer4Total     = tf.add(layer4Transpose, layer4Convolv)
 	
     layer3Transpose = tf.layers.conv2d_transpose(layer4Total, num_classes, 4, strides= (2, 2), padding= 'same', 
-												 kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
-    
-    layer3Convolv = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding= 'same',
-                                   kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+                                                 kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
 
-    layer3Total = tf.add(layer3Transpose, layer3Convolv)
+    layer3Convolv   = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding= 'same',
+                                       kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+
+    layer3Total     = tf.add(layer3Transpose, layer3Convolv)
     
-	finalLayer = tf.layers.conv2d_transpose(layer3Total, num_classes, 16, strides= (8, 8), padding= 'same',  
-                                               kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    finalLayer      = tf.layers.conv2d_transpose(layer3Total, num_classes, 16, strides= (8, 8), padding= 'same',  
+                                                 kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
     return finalLayer
 tests.test_layers(layers)
 
@@ -95,16 +92,14 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-	correct_label = tf.reshape(correct_label, (-1,num_classes) )
+    correct_label = tf.reshape(correct_label, (-1,num_classes) )
     logits = tf.reshape(nn_last_layer, (-1, num_classes) )
-
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits( logits = logits, labels = correct_label ))
     optimizer = tf.train.AdamOptimizer( learning_rate = learning_rate )
     train_op = optimizer.minimize(cross_entropy_loss)
 
     return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
-
 
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op,
              cross_entropy_loss, input_image, correct_label, keep_prob, learning_rate):
@@ -143,7 +138,6 @@ def run():
     helper.maybe_download_pretrained_vgg(data_dir)
 
     with tf.Session() as sess:
-
         vgg_path = os.path.join(data_dir, 'vgg')
         get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
  
@@ -162,9 +156,9 @@ def run():
         # Saving data with helper.save_inference_samples
         helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image )
 
-        print("Elapsed Time...")
-        end = time.time()
-        print(end - start)
+    print("Elapsed Time...")
+    end = time.time()
+    print(end - start)
 
 if __name__ == '__main__':
     run()
